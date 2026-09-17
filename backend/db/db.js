9e864e0e6b92ff6fe70080893f95c08cfee2d42a
Aman Ruhela <amank004433@gmail.com>
2026-09-17 16:40:20 +0530

    Fix database error

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 
-async function dbConnection(req,res) {
+async function dbConnection() {
   try {
     const connection = await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDb connected successfully");
   } catch (error) {
-    res.status(500).json({ error: error });
+    console.error("Database connection failed:",error);
+    
   }
 }
 
