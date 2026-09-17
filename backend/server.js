ed27ff8d8a75f97581968f0625441c0bc008cc3c
Aman Ruhela <amank004433@gmail.com>
2026-09-17 16:09:45 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin:[ "https://spotify-six-self.vercel.app", "http://localhost:5174"],
+    origin:[ "https://spotify-six-self.vercel.app", "http://localhost:5174/"],
     credentials: true,
   }),
 );
