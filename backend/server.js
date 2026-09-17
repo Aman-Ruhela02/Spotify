1d104cbb3b02b928525a19963befa790cea492e7
Aman Ruhela <amank004433@gmail.com>
2026-09-17 16:21:46 +0530

    Fix production CORS

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,12 @@ app.use(
       }
     },
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+app.options("*", cors());
 
 app.use(express.json());
 app.use(cookieParser());
